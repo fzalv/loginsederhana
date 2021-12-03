680aeb9da581b3608c1cb5b6abdfad11e68b39ff
Fauzi Alvan Fadillah <youhaveamessag3@gmail.com>
2021-12-03 10:22:28 +0700

    menu management

--- application/views/templates/sidebar.php
+++ application/views/templates/sidebar.php
@@ -12,34 +12,51 @@
     <!-- Divider -->
     <hr class="sidebar-divider">
 
-    <div class="sidebar-heading">
-        Administrator
-    </div>
+    <!-- QUERY MENU -->
+    <?php
+    $role_id = $this->session->userdata('role_id');
+    $queryMenu = "SELECT user_menu.id, menu
+                FROM user_menu JOIN user_access_menu
+                ON user_menu.id = user_access_menu.menu_id
+                WHERE user_access_menu.role_id = $role_id
+                ORDER BY user_access_menu.menu_id ASC";
 
-    <!-- Nav Item - Dashboard -->
-    <li class="nav-item">
-        <a class="nav-link" href="index.html">
-            <i class="fas fa-fw fa-tachometer-alt"></i>
-            <span>Dashboard</span></a>
-    </li>
+    $menu = $this->db->query($queryMenu)->result_array();
 
-    <!-- Divider -->
+    ?>
+
+    <!-- LOOPING MENU -->
+    <?php foreach ($menu as $m) : ?>
+        <div class="sidebar-heading">
+            <?= $m['menu']; ?>
+        </div>
+
+        <!-- SIAPKAN SUBMENU SESUAI MENU -->
+        <?php
+        $menuId = $m['id'];
+        $querySubMenu = "SELECT *
+                    FROM user_sub_menu
+                    WHERE menu_id = $menuId
+                    AND is_active = 1";
+
+        $subMenu = $this->db->query($querySubMenu)->result_array();
+        ?>
+
+        <?php foreach ($subMenu as $sm) : ?>
+
+            <!-- Nav Item - Dashboard -->
+            <li class="nav-item">
+                <a class="nav-link" href="<?= base_url($sm['url']); ?>">
+                    <i class="<?= $sm['icon']; ?>"></i>
+                    <span><?= $sm['title']; ?></span></a>
+            </li>
+
+        <?php endforeach; ?>
+
+
+    <?php endforeach ?>
+
     <hr class="sidebar-divider">
-
-    <!-- Heading -->
-    <div class="sidebar-heading">
-        User
-    </div>
-
-    <!-- Nav Item - Charts -->
-    <li class="nav-item">
-        <a class="nav-link" href="charts.html">
-            <i class="fas fa-fw fa-chart-area"></i>
-            <span>My Profile</span></a>
-    </li>
-
-    <!-- Divider -->
-    <hr class="sidebar-divider d-none d-md-block">
 
     <!-- Sidebar Toggler (Sidebar) -->
     <div class="text-center d-none d-md-inline">
